79dec2c3d0dc97879bc2f1cf6f1fd6e33fea8c07
liebesu <lichunjiang42@gmail.com>
2016-03-08 13:32:48 +0800

    update
    update

--- get_pe_info.py
+++ get_pe_info.py
@@ -1,2 +1,10 @@
+import os
+
 __author__ = 'liebesu'
 import pefile
+import requests
+def openurl(sha256):
+    url='https://www.virustotal.com/en/file/'
+    newurl=os.path.j
+
+if __name__=="__main__":
