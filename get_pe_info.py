343e966355662b260c14257d9f90a73976280432
liebesu <lichunjiang42@gmail.com>
2016-03-09 17:54:02 +0800

    test
    test

--- get_pe_info.py
+++ get_pe_info.py
@@ -1,19 +1,59 @@
+# -*- encoding:utf-8 -*-
+import cookielib
 import os
+import urllib2
 
 __author__ = 'liebesu'
 import pefile
 import requests
-from bs4 import beautiful
-def openurl(sha256):
-    urllong='https://www.virustotal.com/en/file/4dd2e027e5eb580efcb7b08a5250cbb0c4de78b31697a9009faf0ee980bda041/analysis/'
+from bs4 import BeautifulSoup
+def openurl():
+    url='https://www.virustotal.com/en/file/4dd2e027e5eb580efcb7b08a5250cbb0c4de78b31697a9009faf0ee980bda041/analysis/'
+    print "1"
+    response=urllib2.urlopen(url)
+    print response.getcode()
+    print len(response.read())
+    print "2"
+    request = urllib2.Request(url)
+    request.add_header("user-agent","Mozilla/5.0")
+    response2=urllib2.urlopen(request)
+    print response2.getcode()
+    print len(response2.read())
+    print "3"
+    cj=cookielib.CookieJar()
+    opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    urllib2.install_opener(opener)
+    response3=urllib2.urlopen(request)
+    print response3.getcode()
+    print len(response3.read())
+    print cj
+    print "4"
     r=requests.session()
-    parms={
-        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
-        'Referer':urllong
-    }
+    s=r.get(url)
 
-    r.post(urllong,parms=parms)
-    print r.text()
+
+    print "5"
+
+
+    #Cookie 容器
+    __cookie = cookielib.CookieJar()
+    __req = urllib2.build_opener(urllib2.HTTPCookieProcessor(__cookie))
+    urllib2.install_opener(__req)
+
+    #先请求一下首页, 得到Cookie
+    urllib2.urlopen(url)
+
+    #然后再请求查询
+    response = urllib2.urlopen(url)
+    html=response.read()
+
+    #输出结果
+    print html
+    print "6"
+    f=requests.get(url)
+    f=str(f)
+    print f
 
 if __name__=="__main__":
+    openurl()
 
