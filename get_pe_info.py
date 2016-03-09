514feb9c63c4f698e42ffa6313bc90392fbc80e7
liebesu <lichunjiang42@gmail.com>
2016-03-09 11:29:19 +0800

    update
    update

--- get_pe_info.py
+++ get_pe_info.py
@@ -3,8 +3,17 @@ import os
 __author__ = 'liebesu'
 import pefile
 import requests
+from bs4 import beautiful
 def openurl(sha256):
-    url='https://www.virustotal.com/en/file/'
-    newurl=os.path.j
+    urllong='https://www.virustotal.com/en/file/4dd2e027e5eb580efcb7b08a5250cbb0c4de78b31697a9009faf0ee980bda041/analysis/'
+    r=requests.session()
+    parms={
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+        'Referer':urllong
+    }
+
+    r.post(urllong,parms=parms)
+    print r.text()
 
 if __name__=="__main__":
+
