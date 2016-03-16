200277ec32dc2fdb4eacae04630dd7f3ffb2f320
liebesu <lichunjiang42@gmail.com>
2016-03-16 17:53:56 +0800

    update
    update

--- get_pe_info.py
+++ get_pe_info.py
@@ -1,30 +1,25 @@
-# -*- encoding:utf-8 -*-
-import cookielib
 import httplib
-import os
-import urllib
-import urllib2
-
-__author__ = 'liebesu'
-import pefile
-import requests
-from bs4 import BeautifulSoup
-s=requests.session()
-longurl='https://www.virustotal.com/en/file/634f60864019a56185191d387e69faecead42a978075d844aa2deca8e7987c4a/analysis/'
-#apiurl='https://www.virustotal.com/en/user/'+param['username']+'/apikey/'
-headers = { "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            'accept-encoding':'gzip, deflate, sdch',
-            'accept-language':'zh-CN,zh;q=0.8,zh-TW;q=0.6'}
-parms={ 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
-           'Referer': 'https://www.virustotal.com/en/file/6076632ad5b0c3dedd84769a9b6f13bff1723a2bfa4ab75820aa61df7b33b458/analysis/',
-           'Content-Type': 'application/x-www-form-urlencoded',}
-cookies={'VT_PREFERRED_LANGUAGE=en'}
-r=s.get(longurl,verify=True)
-print r.cookies
-print len(r.text)
-print r.text
-if r.status_code == 200:
-    page=r.text.encode('ascii', 'ignore')
-    print page
+import BeautifulSoup
+def get_page():
+    headers = {
+               'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
+               'Referer': 'https://www.virustotal.com/en/',
 
 
+            }
+    conn = httplib.HTTPSConnection("www.virustotal.com")
+    conn.request(method='GET', url='/en/file/4ee9e741d65680cd4c44b5e9a5d7636511c0ae4b3e774b454b17fdc675edf289/analysis/',
+                     headers=headers)
+    response = conn.getresponse()
+    if response.status == 200:
+        HTML=response.read()
+        print len(HTML)
+        a=open("html1",'a')
+        a.write(HTML)
+        a.close()
+def convert_to_json():
+    pass
+
+    #soup=BeautifulSoup(HTML,'html.parser')
+if __name__=="__main__":
+    get_page()
