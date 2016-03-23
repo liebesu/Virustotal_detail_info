86987fa86558f2c0edc3c72ffe37d937fcf4c5eb
liebesu <lichunjiang42@gmail.com>
2016-03-23 18:13:16 +0800

    update
    update

--- get_pe_info.py
+++ get_pe_info.py
@@ -1,6 +1,9 @@
 import httplib
 from bs4 import BeautifulSoup
 import json
+from requests.packages import chardet
+
+
 def get_page():
     headers = {
                'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
@@ -16,16 +19,32 @@ def get_page():
 def convert_to_json(page_data):
     jsons={}
     soup=BeautifulSoup(page_data,"html.parser")
-    keynum=len(soup.find_all(class_='field-key'))
-    print keynum
+    soup_details=soup.find(id="file-details")
+
+    soup_h5=soup_details.h5
+    soup_key=soup_details.span
+    soup_value=soup_details.span.parent
+    key=soup_value.get_text().replace(soup_key.get_text(),"")
+    result={soup_h5.get_text():{soup_key.get_text():key}}
+    print result
+    '''print soup.h5.next_sibling.get_text()
+    if ' PE imports' in  [h5.text for h5 in soup.find_all('h5')]:
+        pass
+
+    first=soup.h5
+    se=first.next_sibling
+    print se.children
+'''
+        #setions=soup.find_all(class_='')
+
 
 
 
     #h5s=soup.find('h5')
 
     #spans=soup.find_all('span','field_key')
-    classs=soup.find(id="file-details")
-    inner_items = [li.text.strip() for li in classs.find_all('h5')]
+    '''classs=soup.find(id="file-details")
+    inner_items = [li.text.strip() for li in classs.find_all('h5')]'''
 
     #print h5s.get_text()
     '''for text1 in h5s:
