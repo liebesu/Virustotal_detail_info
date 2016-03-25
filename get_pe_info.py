49cae59895a64430971ad96444a052593757b1ac
liebesu <lichunjiang42@gmail.com>
2016-03-25 18:32:39 +0800

    update
    update

--- get_pe_info.py
+++ get_pe_info.py
@@ -19,17 +19,24 @@ def get_page():
 def convert_to_json(page_data):
     jsons={}
     soup=BeautifulSoup(page_data,"html.parser")
-    soup_details=soup.find(id="file-details")
-    souptest=soup.find_all(class_="field-key","h5")
-    print [soup2 for soup2 in souptest.get_text()]
-    soup_h5=soup_details.h5
-    soup_key=soup_details.span
-    soup_value=soup_details.span.parent
-    key=soup_value.get_text().replace(soup_key.get_text(),"").replace("\n","").replace("u","")
-    result1={soup_h5.get_text():{soup_key.get_text():key}}
-    jsons['pedetail']=result1
-    print jsons
-    '''print soup.h5.next_sibling.get_text()
+    soup_details=soup.find(id='file-details')
+    soup_details1=BeautifulSoup(str(soup_details),"html.parser")
+    enum_cons=soup_details1.find_all(class_="enum-container")
+    for enum_con in enum_cons:
+        #print soup_detail
+        h5=enum_con.previous_sibling.previous_sibling.get_text()
+        key=enum_con.span.get_text()
+        value=enum_con.get_text().replace(key,"")
+        print h5
+        print key
+        print value
+        '''soup_key=soup_details.span
+        soup_value=soup_details.span.parent
+        key=soup_value.get_text().replace(soup_key.get_text(),"").replace("\n","").replace("u","")
+        result1={soup_h5.get_text():{soup_key.get_text():key}}
+        jsons[soup_h5]=result1
+        print jsons
+    print soup.h5.next_sibling.get_text()
     if ' PE imports' in  [h5.text for h5 in soup.find_all('h5')]:
         pass
 
